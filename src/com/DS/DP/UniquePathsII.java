45cfe14a68856e82f989002eec34161a431c9cc9
Rohit Sharma <rohituks@gmail.com>
2021-05-05 23:25:22 -0400

    updated code

--- src/com/DS/DP/UniquePathsII.java
+++ src/com/DS/DP/UniquePathsII.java
@@ -16,38 +16,38 @@ public class UniquePathsII {
     public static int usingTabulation(int[][] inputgrid){
         int m = inputgrid.length;
         int n = inputgrid[0].length;
+
         int[][] table = new int[m][n];
-        //For First cell- If first cell has obstacle, we cannot proceed
-        if(inputgrid[0][0] == 1){
+        //For first cell
+        if(inputgrid[0][0] ==1){
             return 0;
         }
-        //for First column
-        for(int row=1; row <= m-1; row++){
+        //for first column
+        for( int row=1; row<m; row++){
             if(inputgrid[row][0]==1){
                 break;
             }
-            table[row][0]=1;
+            table[row][0] =1;
         }
 
-        for(int col=1; col <=n-1; col++){
+        //for rows
+        for(int col=1; col<n; col++){
             if(inputgrid[0][col]==1){
                 break;
             }
             table[0][col] =1;
         }
-
-        for(int i=1;i<m; i++){
+        for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 if(inputgrid[i][j]==1){
-                    table[i][j]=0;
+                    table[i][j] = 0;
                 }
                 else
-                {
-                    table[i][j] = table[i-1][j] + table[i][j-1];
+                    {
+                    table[i][j] = table[i-1][j] +table[i][j-1];
                 }
             }
         }
-
         return table[m-1][n-1];
     }
 
