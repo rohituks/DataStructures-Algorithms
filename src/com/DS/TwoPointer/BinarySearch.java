4accec7865642d27bf206711357ea4cc04209293
Rohit Sharma <rohituks@gmail.com>
2022-04-25 08:57:22 -0400

    Two pointer problem solutions

--- src/com/DS/TwoPointer/BinarySearch.java
+++ src/com/DS/TwoPointer/BinarySearch.java
@@ -6,11 +6,14 @@ public class BinarySearch {
     public static int twopointerApproach(int[] arr, int aTarget){
         int i =0;
         int j = arr.length -1;
-        while(i<j){
+
+        while(i<=j){//for loop
+
             int midindex = (i+j)/2;
+            ///
             if(arr[midindex] == aTarget){
                 return midindex;
-            } else if (midindex < aTarget){
+            } else if (arr[midindex] < aTarget){
                 i++;
             } else {
                 j--;
@@ -23,7 +26,7 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] input = new int[]{1,2,3,7,8,18};
-        int target = 8;
+        int target = 18;
         System.out.println(twopointerApproach(input, target));
         
     }
--- src/com/DS/TwoPointer/TwoSum.java
+++ src/com/DS/TwoPointer/TwoSum.java
@@ -2,18 +2,16 @@ package com.DS.TwoPointer;
 
 import java.util.Arrays;
 
-import javax.print.event.PrintJobListener;
-
-import ch.qos.logback.core.joran.conditional.ElseAction;
 
 public class TwoSum {
 
 
     public static int[] TwoSumBruteForce(int[] arr, int aTarget){
-        //Brute force way
+        //Brute force way O(n2)
         //int[] result = new int[2]; //1
         int i = 0; //start pointer
-        for(i =0; i < arr.length; i++){ //n
+        for(; i < arr.length; i++){ //n
+
             for(int j = i+1; j < arr.length; j++){ //n
                 if (arr[j] == aTarget - arr[i]){ //1 
                     return new int[] {i,j}; //1
@@ -37,11 +35,12 @@ public class TwoSum {
     public static int[] TwoSumTwoPointers(int[] arr, int aTarget){
         //Two pointer way
         //Multiple pointers-> To keep track of multiple indices of the input.e.g., looking at two different indices at the same time.
-        //int[] result = new int[2]; //1
+        //int[] result = new int[2]; // 1
         int i = 0; //start pointer
         int j = arr.length - 1;
-        for(i =0; i < arr.length; i++){ //n // we can use while loop as well, Doesnt matter
+        for(; i < arr.length; i++){ //n // we can use while loop as well, Doesnt matter while(i<j)
             // This gap is very important, because depending upon problem we can write conidtions here
+
             if(arr[i] + arr[j]== aTarget){
                     return new int[] {i,j};
             }
